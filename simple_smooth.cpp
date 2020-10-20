44e022cabb81459c584e14e43d330093d056d92a
helywin <jiang770882022@hotmail.com>
2020-10-20 20:21:45 +0800

    add original method doing filtration

--- simple_smooth.cpp
+++ simple_smooth.cpp
@@ -17,27 +17,33 @@ int main()
 
     Mat origin = imread("../assets/dark_character.jpg", IMREAD_GRAYSCALE);
 //    std::cout << "mat type: " << origin.type();
-//    Mat expand;
-    Mat after_box, after_gauss;
+    Mat expand;
+    Mat after_box = Mat::ones(origin.size(), CV_8U);
     int size = 3;
-//    int border = size / 2;
-//    copyMakeBorder(origin, expand, border, border, border, border, BORDER_CONSTANT);
-//    expand.convertTo(expand, CV_32F);
+    int border = size / 2;
+    copyMakeBorder(origin, expand, border, border, border, border, BORDER_CONSTANT);
+    expand.convertTo(expand, CV_32F);
     const Mat box_kernel = Mat::ones(size, size, CV_32F) / size / size;
-    filter2D(origin, after_box, -1, box_kernel);
+//    filter2D(origin, after_box, -1, box_kernel);
+    for (int i = 0; i < origin.rows; ++i) {
+        for (int j = 0; j < origin.cols; ++j) {
+            after_box.at<uchar>(i, j) = (uchar) expand(Rect{j, i, size, size}).dot(box_kernel);
+        }
+    }
 
-    Mat guass_kernel = Mat::ones(size, size, CV_32F);
-    guass_kernel.at<float>(0,0) = 0.3679;
-    guass_kernel.at<float>(0,1) = 0.6065;
-    guass_kernel.at<float>(0,2) = 0.3679;
-    guass_kernel.at<float>(1,0) = 0.6065;
-    guass_kernel.at<float>(1,1) = 1.0;
-    guass_kernel.at<float>(1,2) = 0.6065;
-    guass_kernel.at<float>(2,0) = 0.3679;
-    guass_kernel.at<float>(2,1) = 0.6065;
-    guass_kernel.at<float>(2,2) = 0.3679;
-    guass_kernel /= 4.8976;
-    filter2D(origin, after_gauss, -1, guass_kernel);
+    Mat gauss_kernel = Mat::ones(size, size, CV_32F);
+    gauss_kernel.at<float>(0,0) = 0.3679;
+    gauss_kernel.at<float>(0,1) = 0.6065;
+    gauss_kernel.at<float>(0,2) = 0.3679;
+    gauss_kernel.at<float>(1,0) = 0.6065;
+    gauss_kernel.at<float>(1,1) = 1.0;
+    gauss_kernel.at<float>(1,2) = 0.6065;
+    gauss_kernel.at<float>(2,0) = 0.3679;
+    gauss_kernel.at<float>(2,1) = 0.6065;
+    gauss_kernel.at<float>(2,2) = 0.3679;
+    gauss_kernel /= 4.8976;
+    Mat after_gauss;
+    filter2D(origin, after_gauss, -1, gauss_kernel);
 
     after_box.convertTo(after_box, CV_8U);
     after_gauss.convertTo(after_gauss, CV_8U);
